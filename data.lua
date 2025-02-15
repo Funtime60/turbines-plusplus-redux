f7ebfc927f3be10545dcce4b81d2be5d11bbdf72
Funtime60 <24595923+Funtime60@users.noreply.github.com>
2025-02-14 21:27:42 -0700

    Remove unused SE or otherwise files.

--- data.lua
+++ data.lua
@@ -1,21 +1,3 @@
 
---[[
---SE
-if mods["space-exploration"] then
-    require('scripts/SE/data')
-else
-    require('prototypes/tur_main')
-end
-
-if mods["CompressedFluids"] then
-	compressedFluids.remote.forceCompressingFluid("steam")
-	compressedFluids.remote.addTemperatureToFluid("steam", 15)
-	compressedFluids.remote.addTemperatureToFluid("steam", 165)
-	compressedFluids.remote.addTemperatureToFluid("steam", 500)
-	compressedFluids.remote.addTemperatureToFluid("steam", 750)
-	compressedFluids.remote.addTemperatureToFluid("steam", 900)
-	compressedFluids.remote.addTemperatureToFluid("steam", 1000)
-end
-]]--
 require('prototypes.tur_main')
 require("prototypes/condensation_recipes")
